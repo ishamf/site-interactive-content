f878fab8af35e4738f473484491c480037f654b1
Isham Faizal <ishamf@users.noreply.github.com>
2024-12-18 23:03:55 +0700

    Format code

--- packages/purescript-fns/src/Calculator.purs
+++ packages/purescript-fns/src/Calculator.purs
@@ -9,8 +9,7 @@ module Calculator
   , sortIpRanges
   , subtractIpRanges
   , validateIPRanges
-  )
-  where
+  ) where
 
 import Prelude
 
@@ -27,62 +26,70 @@ import JS.BigInt (BigInt, and, fromInt, or, shl, shr)
 import JS.BigInt as BigInt
 import Partial.Unsafe (unsafeCrashWith)
 
-type IPRange = {
-    start :: BigInt,
-    end :: BigInt
-}
+type IPRange =
+  { start :: BigInt
+  , end :: BigInt
+  }
 
 fromRightWithError :: forall t e. String -> Either e t -> t
 fromRightWithError error (Left _) = unsafeCrashWith error
 fromRightWithError _ (Right r) = r
 
 ipRangeRegex :: Regex
-ipRangeRegex = 
-    fromRightWithError "Cannot parse regex" ( 
-        regex "([b0-b9]+)\\.([b0-b9]+)\\.([b0-b9]+)\\.([b0-b9]+)/([b0-b9]+)" (parseFlags "") 
+ipRangeRegex =
+  fromRightWithError "Cannot parse regex"
+    ( regex "([b0-b9]+)\\.([b0-b9]+)\\.([b0-b9]+)\\.([b0-b9]+)/([b0-b9]+)" (parseFlags "")
     )
 
-
 calc :: String -> String -> Either String String
-calc allowedStr disallowedStr = do 
-    allowed <- parseIPRanges allowedStr
-    disallowed <- parseIPRanges disallowedStr
-    Right (renderIpRanges (
-        subtractIpRanges (cleanupIpRanges allowed) (cleanupIpRanges disallowed)
-    ))
+calc allowedStr disallowedStr = do
+  allowed <- parseIPRanges allowedStr
+  disallowed <- parseIPRanges disallowedStr
+  Right
+    ( renderIpRanges
+        ( subtractIpRanges (cleanupIpRanges allowed) (cleanupIpRanges disallowed)
+        )
+    )
 
 parseIPRange :: String -> Either String IPRange
 parseIPRange str = do
-    m <- note ("Unable to match " <> str <> " as an IP range" ) (match ipRangeRegex str)
-    matches <- case toArray m of
-      [_, Just a1, Just a2, Just a3, Just a4, Just l] -> Right {address: [a1, a2, a3, a4], length: l}
-      _ -> Left "Cannot match regex"
-    address <- let f acc s = do 
-                                a <- acc
-                                i <- note "Cannot parse int" (Int.fromString s)
-                                if i < 0 || i > 255
-                                then Left "Invalid IP address component"
-                                else Right (shl a b8 + fromInt i)
-                in (foldl f (Right b0) matches.address)
-    length <- note "Cannot parse int" (BigInt.fromString matches.length)
-    Right (let mask = (shl b1 (b32 - length)) - b1 
-            in {start: and address (BigInt.not mask), end: or address mask})
+  m <- note ("Unable to match " <> str <> " as an IP range") (match ipRangeRegex str)
+  matches <- case toArray m of
+    [ _, Just a1, Just a2, Just a3, Just a4, Just l ] -> Right { address: [ a1, a2, a3, a4 ], length: l }
+    _ -> Left "Cannot match regex"
+  address <-
+    let
+      f acc s = do
+        a <- acc
+        i <- note "Cannot parse int" (Int.fromString s)
+        if i < 0 || i > 255 then Left "Invalid IP address component"
+        else Right (shl a b8 + fromInt i)
+    in
+      (foldl f (Right b0) matches.address)
+  length <- note "Cannot parse int" (BigInt.fromString matches.length)
+  Right
+    ( let
+        mask = (shl b1 (b32 - length)) - b1
+      in
+        { start: and address (BigInt.not mask), end: or address mask }
+    )
 
 parseIPRanges :: String -> Either String (List IPRange)
-parseIPRanges = 
-    split (Pattern ",") >>> 
-    map (trim >>> parseIPRange) >>> 
-    (foldr (lift2 Cons) (Right Nil))
-
+parseIPRanges =
+  split (Pattern ",")
+    >>> map (trim >>> parseIPRange)
+    >>>
+      (foldr (lift2 Cons) (Right Nil))
 
 sortIpRanges :: List IPRange -> List IPRange
 sortIpRanges = sortBy cmp
-    where cmp a b = compare (a.start) (b.start)
+  where
+  cmp a b = compare (a.start) (b.start)
 
 mergeIpRanges :: List IPRange -> List IPRange
-mergeIpRanges (a:b:xs) = if a.end >= b.start - b1
-                         then {start: a.start, end: b.end} : mergeIpRanges xs
-                         else a:(mergeIpRanges (b:xs))
+mergeIpRanges (a : b : xs) =
+  if a.end >= b.start - b1 then { start: a.start, end: b.end } : mergeIpRanges xs
+  else a : (mergeIpRanges (b : xs))
 mergeIpRanges xs = xs
 
 cleanupIpRanges :: List IPRange -> List IPRange
@@ -91,64 +98,73 @@ cleanupIpRanges = sortIpRanges >>> mergeIpRanges
 subtractIpRanges :: List IPRange -> List IPRange -> List IPRange
 subtractIpRanges Nil _ = Nil
 subtractIpRanges a Nil = a
-subtractIpRanges (a:as) (b:bs) = 
-    if a.end < b.start
-    then a : subtractIpRanges as (b:bs)
-    else if b.end < a.start
-    then subtractIpRanges (a:as) bs
-    else -- a and b definitely intersects
-    if b.start <= a.start && a.end <= b.end
-    then subtractIpRanges as (b:bs)
-    else if a.start < b.start && b.end < a.end
-    then {start: a.start, end: b.start - b1} : {start: b.end + b1, end: a.end} : subtractIpRanges as bs
-    else if b.start <= a.start
-    then {start: b.end + b1, end: a.end} : subtractIpRanges as bs
-    else --  b.end >= a.end
-         {start: a.start, end: b.start - b1} : subtractIpRanges as (b:bs)
+subtractIpRanges (a : as) (b : bs) =
+  if a.end < b.start then a : subtractIpRanges as (b : bs)
+  else if b.end < a.start then subtractIpRanges (a : as) bs
+  else -- a and b definitely intersects
+    if b.start <= a.start && a.end <= b.end then subtractIpRanges as (b : bs)
+  else if a.start < b.start && b.end < a.end then { start: a.start, end: b.start - b1 } : { start: b.end + b1, end: a.end } : subtractIpRanges as bs
+  else if b.start <= a.start then { start: b.end + b1, end: a.end } : subtractIpRanges as bs
+  else --  b.end >= a.end
+
+    { start: a.start, end: b.start - b1 } : subtractIpRanges as (b : bs)
 
 renderIpRanges :: List IPRange -> String
 renderIpRanges = renderIpRanges' >>> fromFoldable >>> joinWith ", "
-    where 
-        renderIpRanges' Nil = Nil
-        renderIpRanges' (x:xs) = concat ((renderRange x) : (renderIpRanges' xs) : Nil)
-        renderRange x = let block_length = maxBlockLength x.start x.end
-                            block_end = x.start + shl b1 block_length - b1
-                        in 
-                            (renderPrefix x.start <> "/" <> show (b32 - block_length)) :
-                            (if block_end == x.end
-                             then Nil
-                             else (renderRange {start: block_end + b1, end: x.end}))
-        renderPrefix n = joinWith "." $ fromFoldable (map (sc >>> show) (map fromInt $ range 1 4))
-            where sc i = let shn = (b4 - i) * b8
-                         in shr (and n (shl b255 shn)) shn
+  where
+  renderIpRanges' Nil = Nil
+  renderIpRanges' (x : xs) = concat ((renderRange x) : (renderIpRanges' xs) : Nil)
+  renderRange x =
+    let
+      block_length = maxBlockLength x.start x.end
+      block_end = x.start + shl b1 block_length - b1
+    in
+      (renderPrefix x.start <> "/" <> show (b32 - block_length)) :
+        ( if block_end == x.end then Nil
+          else (renderRange { start: block_end + b1, end: x.end })
+        )
+  renderPrefix n = joinWith "." $ fromFoldable (map (sc >>> show) (map fromInt $ range 1 4))
+    where
+    sc i =
+      let
+        shn = (b4 - i) * b8
+      in
+        shr (and n (shl b255 shn)) shn
 
 maxBlockLength ∷ BigInt → BigInt → BigInt
 maxBlockLength start end = mbl start end b1
-    where
-        mbl s e i = let mask = shl b1 i - b1 in
-                    if and s mask == b0 && s + mask <= e
-                    then mbl s e (i + b1)
-                    else i - b1
+  where
+  mbl s e i =
+    let
+      mask = shl b1 i - b1
+    in
+      if and s mask == b0 && s + mask <= e then mbl s e (i + b1)
+      else i - b1
 
 calcAsRecord :: String -> String -> { error :: String, result :: String }
-calcAsRecord a b = case (calc a b) of 
-    Right result -> { result, error: "" }
-    Left error -> {result: "", error }
+calcAsRecord a b = case (calc a b) of
+  Right result -> { result, error: "" }
+  Left error -> { result: "", error }
 
 validateIPRanges :: String -> String
 validateIPRanges s = case parseIPRanges s of
-    Right _ -> ""
-    Left message -> message
+  Right _ -> ""
+  Left message -> message
 
 b0 :: BigInt
 b0 = fromInt 0
+
 b1 :: BigInt
 b1 = fromInt 1
+
 b4 :: BigInt
 b4 = fromInt 4
+
 b8 :: BigInt
 b8 = fromInt 8
+
 b32 :: BigInt
 b32 = fromInt 32
+
 b255 :: BigInt
 b255 = fromInt 255
--- packages/purescript-fns/test/Main.purs
+++ packages/purescript-fns/test/Main.purs
@@ -18,64 +18,70 @@ main = do
   runTest do
     suite "parseIpRange" do
       test "0.0.0.0/0" do
-        Assert.equal (Right {start: fromInt 0, end: (shl (fromInt 1) (fromInt 32)) - fromInt 1}) $ parseIPRange "0.0.0.0/0"
+        Assert.equal (Right { start: fromInt 0, end: (shl (fromInt 1) (fromInt 32)) - fromInt 1 }) $ parseIPRange "0.0.0.0/0"
       test "10.0.0.0/8" do
-        Assert.equal (Right {start: (shl (fromInt 10) (fromInt 24)), end: (shl (fromInt 11) (fromInt 24) - (fromInt 1))}) $ parseIPRange "10.0.0.0/8"
+        Assert.equal (Right { start: (shl (fromInt 10) (fromInt 24)), end: (shl (fromInt 11) (fromInt 24) - (fromInt 1)) }) $ parseIPRange "10.0.0.0/8"
 
     suite "parseIPRanges" do
       test "works" do
-        (rightAndEqual
-          (do i1 <- parseIPRange "10.0.0.0/8"
-              i2 <- parseIPRange "11.0.0.0/8"
-              Right (List.fromFoldable [i1, i2])
-          ) 
-          (parseIPRanges "10.0.0.0/8, 11.0.0.0/8")  
+        ( rightAndEqual
+            ( do
+                i1 <- parseIPRange "10.0.0.0/8"
+                i2 <- parseIPRange "11.0.0.0/8"
+                Right (List.fromFoldable [ i1, i2 ])
+            )
+            (parseIPRanges "10.0.0.0/8, 11.0.0.0/8")
         )
 
     suite "sortIPRanges" do
       test "works" do
-        (rightAndEqual 
-          (parseIPRanges "10.0.0.0/8, 11.0.0.0/8") 
-          ((map sortIpRanges) (parseIPRanges "11.0.0.0/8, 10.0.0.0/8")))
+        ( rightAndEqual
+            (parseIPRanges "10.0.0.0/8, 11.0.0.0/8")
+            ((map sortIpRanges) (parseIPRanges "11.0.0.0/8, 10.0.0.0/8"))
+        )
 
     suite "mergeIPRanges" do
       test "works" do
-        (rightAndEqual
-          (parseIPRanges "10.0.0.0/8")
-          ((map mergeIpRanges) (parseIPRanges "10.0.0.0/9, 10.128.0.0/9"))
+        ( rightAndEqual
+            (parseIPRanges "10.0.0.0/8")
+            ((map mergeIpRanges) (parseIPRanges "10.0.0.0/9, 10.128.0.0/9"))
         )
 
     suite "subtractIpRanges" do
       test "test1" do
-        (rightAndEqual
-          (parseIPRanges "10.0.0.128/25,10.0.1.128/25")
-          ((lift2 subtractIpRanges) 
-            (parseIPRanges "10.0.0.0/24,10.0.1.0/24") 
-            (parseIPRanges "10.0.0.0/25,10.0.1.0/25"))
+        ( rightAndEqual
+            (parseIPRanges "10.0.0.128/25,10.0.1.128/25")
+            ( (lift2 subtractIpRanges)
+                (parseIPRanges "10.0.0.0/24,10.0.1.0/24")
+                (parseIPRanges "10.0.0.0/25,10.0.1.0/25")
+            )
         )
       test "test2" do
-        (rightAndEqual
-          (parseIPRanges "128.0.0.0/1")
-          ((lift2 subtractIpRanges) 
-            (parseIPRanges "0.0.0.0/0") 
-            (parseIPRanges "0.0.0.0/1"))
+        ( rightAndEqual
+            (parseIPRanges "128.0.0.0/1")
+            ( (lift2 subtractIpRanges)
+                (parseIPRanges "0.0.0.0/0")
+                (parseIPRanges "0.0.0.0/1")
+            )
         )
 
     suite "renderIpRanges" do
       test "test1" do
-        (rightAndEqual
-          (Right "10.0.0.128/25")
-          ((map renderIpRanges) (parseIPRanges "10.0.0.128/25"))
+        ( rightAndEqual
+            (Right "10.0.0.128/25")
+            ((map renderIpRanges) (parseIPRanges "10.0.0.128/25"))
         )
 
       test "test2" do
-        (rightAndEqual 
-          (Right "0.0.0.0/2, 64.0.0.0/3, 96.0.0.0/4, 112.0.0.0/5, 120.0.0.0/6, 124.0.0.0/7, 126.0.0.0/8, 127.0.0.0/32, 127.0.0.2/31, 127.0.0.4/30, 127.0.0.8/29, 127.0.0.16/28, 127.0.0.32/27, 127.0.0.64/26, 127.0.0.128/25, 127.0.1.0/24, 127.0.2.0/23, 127.0.4.0/22, 127.0.8.0/21, 127.0.16.0/20, 127.0.32.0/19, 127.0.64.0/18, 127.0.128.0/17, 127.1.0.0/16, 127.2.0.0/15, 127.4.0.0/14, 127.8.0.0/13, 127.16.0.0/12, 127.32.0.0/11, 127.64.0.0/10, 127.128.0.0/9, 128.0.0.0/1")
-          (do
-            allowed <- parseIPRanges "0.0.0.0/0"
-            disallowed <- parseIPRanges "127.0.0.1/32"
+        ( rightAndEqual
+            (Right "0.0.0.0/2, 64.0.0.0/3, 96.0.0.0/4, 112.0.0.0/5, 120.0.0.0/6, 124.0.0.0/7, 126.0.0.0/8, 127.0.0.0/32, 127.0.0.2/31, 127.0.0.4/30, 127.0.0.8/29, 127.0.0.16/28, 127.0.0.32/27, 127.0.0.64/26, 127.0.0.128/25, 127.0.1.0/24, 127.0.2.0/23, 127.0.4.0/22, 127.0.8.0/21, 127.0.16.0/20, 127.0.32.0/19, 127.0.64.0/18, 127.0.128.0/17, 127.1.0.0/16, 127.2.0.0/15, 127.4.0.0/14, 127.8.0.0/13, 127.16.0.0/12, 127.32.0.0/11, 127.64.0.0/10, 127.128.0.0/9, 128.0.0.0/1")
+            ( do
+                allowed <- parseIPRanges "0.0.0.0/0"
+                disallowed <- parseIPRanges "127.0.0.1/32"
 
-            Right (renderIpRanges (subtractIpRanges allowed disallowed))))
+                Right (renderIpRanges (subtractIpRanges allowed disallowed))
+            )
+        )
 
 rightAndEqual ∷ forall a b. Eq a => Eq b => Show a => Show b => Either a b -> Either a b -> Aff Unit
 rightAndEqual a b = do
