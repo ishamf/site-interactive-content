a7195fa36a43d09a2b7e1fdd8b4eaa1c23c50e0e
Isham Faizal <ishamf@users.noreply.github.com>
2024-12-30 23:58:23 +0700

    Add logs to unifyDist

--- scripts/unifyDist.ts
+++ scripts/unifyDist.ts
@@ -2,18 +2,40 @@ import { mkdir, readdir } from "fs/promises";
 import { existsSync } from "fs";
 import { fileURLToPath } from "url";
 import { copy, remove } from "fs-extra";
-import { resolve, dirname } from "path";
+import { resolve, dirname, relative, join } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const resolveFromRoot = (...p: string[]) => resolve(__dirname, "..", ...p);
 
+const root = resolveFromRoot();
+
 const packages = await readdir(resolveFromRoot("packages"));
 
 await remove(resolveFromRoot("dist"));
 await mkdir(resolveFromRoot("dist"));
-await copy(resolveFromRoot("public"), resolveFromRoot("dist"));
+
+function createLogFilter(sourcePath: string) {
+  function logFilter(p: string) {
+    const subPath = relative(sourcePath, p);
+
+    const relSourcePath = relative(root, sourcePath);
+    const relDestPath = relative(root, resolveFromRoot("dist"));
+
+    console.log(join(relDestPath, subPath), "<-", join(relSourcePath, subPath));
+
+    return true;
+  }
+
+  return logFilter;
+}
+
+await copy(resolveFromRoot("public"), resolveFromRoot("dist"), {
+  overwrite: false,
+  errorOnExist: true,
+  filter: createLogFilter(resolveFromRoot("public")),
+});
 
 for (const p of packages) {
   const packageDist = resolveFromRoot("packages", p, "dist");
@@ -21,6 +43,7 @@ for (const p of packages) {
     await copy(packageDist, resolveFromRoot("dist"), {
       overwrite: false,
       errorOnExist: true,
+      filter: createLogFilter(packageDist),
     });
   }
 }
