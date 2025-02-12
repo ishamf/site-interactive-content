38c99284486b8e87653305b6a0363e465ba4a678
Isham Faizal <ishamf@users.noreply.github.com>
2025-02-12 16:50:00 +0700

    Change timestamp test to use utc

--- packages/svelte-4/tests/timestamp-converter.spec.ts
+++ packages/svelte-4/tests/timestamp-converter.spec.ts
@@ -5,11 +5,12 @@ test('Basic timestamp', async ({ page }) => {
   await page.getByRole('button', { name: 'Timestamp Converter' }).click();
   await page.getByRole('textbox', { name: 'Any datetime or timestamp...' }).click();
   await page.getByRole('textbox', { name: 'Any datetime or timestamp...' }).fill('1739347000');
+  await page.getByText('Display in UTC').click();
   await expect(page.locator('xif-timestamp-converter')).toContainText(
     'The type of the input is timestamp (seconds).'
   );
   await expect(page.locator('td').nth(3)).toContainText('Timestamp (milliseconds)');
   await expect(page.locator('td').nth(4)).toContainText('1739347000000');
   await expect(page.locator('td').nth(6)).toContainText('ISO 8601');
-  await expect(page.locator('td').nth(7)).toContainText('2025-02-12T14:56:40.000+07:00');
+  await expect(page.locator('td').nth(7)).toContainText('2025-02-12T07:56:40.000Z');
 });
