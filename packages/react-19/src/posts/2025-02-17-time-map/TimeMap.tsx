7e2508181ce01f9325684edd34d4545230420452
Isham Faizal <ishamf@users.noreply.github.com>
2025-02-21 17:00:27 +0700

    Add slider and alphaSize

--- packages/react-19/src/posts/2025-02-17-time-map/TimeMap.tsx
+++ packages/react-19/src/posts/2025-02-17-time-map/TimeMap.tsx
@@ -2,15 +2,17 @@ import { useRef, useState } from 'react';
 import { DateTime } from 'luxon';
 import { DateTimePicker } from '@mui/x-date-pickers';
 import { MapDisplay } from './components/MapDisplay';
+import { Slider } from '@mui/material';
 
 export function TimeMap() {
   const [time, setTime] = useState<DateTime>(() => DateTime.now());
+  const [sliderValue, setSliderValue] = useState<number>(0);
 
   const containerRef = useRef<HTMLDivElement>(null);
 
   return (
     <div ref={containerRef}>
-      <MapDisplay time={time.valueOf()} />
+      <MapDisplay time={time.valueOf() + (sliderValue * 24 * 3600 * 1000) / 100} />
 
       <DateTimePicker
         value={time}
@@ -21,6 +23,15 @@ export function TimeMap() {
           }
         }}
       />
+
+      <Slider
+        value={sliderValue}
+        onChange={(_e, v) => {
+          if (typeof v === 'number') {
+            setSliderValue(v);
+          }
+        }}
+      ></Slider>
     </div>
   );
 }
--- packages/react-19/src/posts/2025-02-17-time-map/components/MapDisplay/updater.ts
+++ packages/react-19/src/posts/2025-02-17-time-map/components/MapDisplay/updater.ts
@@ -10,21 +10,56 @@ async function loadImages() {
 }
 type MapImageData = Awaited<ReturnType<typeof loadImages>>;
 
-function renderMapAtTime(
-  { dayImageData, nightImageData }: MapImageData,
-  ctx: CanvasRenderingContext2D,
-  time: number
-) {
+function renderMapAtTime({
+  imageData: { dayImageData, nightImageData },
+  ctx,
+  time,
+  alphaSize = 1,
+}: {
+  imageData: MapImageData;
+  ctx: CanvasRenderingContext2D;
+  time: number;
+  alphaSize: number;
+}) {
+  let start = performance.now();
+
   const imageData = ctx.createImageData(canvasWidth, canvasHeight);
+
+  let now = performance.now();
+
+  console.log('createImageData', now - start);
+  start = now;
 
   const getAlpha = createAlphaCalculator(time);
 
-  for (let y = 0; y < canvasHeight; y++) {
-    for (let x = 0; x < canvasWidth; x++) {
-      const latitude = -((y / canvasHeight) * 180 - 90);
-      const longitude = (x / canvasWidth) * 360 - 180;
+  const alphaCache = [];
 
-      const alpha = getAlpha(latitude, longitude);
+  for (let y = 0; y < canvasHeight; y++) {
+    if (alphaSize > 1 && y % alphaSize !== 0) {
+      alphaCache.length = 0;
+    }
+
+    for (let x = 0; x < canvasWidth; x++) {
+      let alpha: number;
+
+      if (alphaSize <= 1) {
+        const latitude = -((y / canvasHeight) * 180 - 90);
+        const longitude = (x / canvasWidth) * 360 - 180;
+
+        alpha = getAlpha(latitude, longitude);
+      } else {
+        const cacheIdx = Math.floor(x / alphaSize);
+
+        if (alphaCache.length < cacheIdx + 1) {
+          const latitude = -((y / canvasHeight) * 180 - 90);
+          const longitude = (x / canvasWidth) * 360 - 180;
+
+          alpha = getAlpha(latitude, longitude);
+          alphaCache[cacheIdx] = alpha;
+        } else {
+          alpha = alphaCache[cacheIdx];
+        }
+      }
 
       for (let i = 0; i < 3; i++) {
         const idx = (y * canvasWidth + x) * 4 + i;
@@ -37,7 +72,14 @@ function renderMapAtTime(
     }
   }
 
+  now = performance.now();
+  console.log('renderMap', now - start);
+  start = now;
+
   ctx.putImageData(imageData, 0, 0);
+
+  now = performance.now();
+  console.log('putImageData', now - start);
 }
 
 export function useMapUpdater(canvasRef: RefObject<HTMLCanvasElement | null>, time: number) {
@@ -55,6 +97,6 @@ export function useMapUpdater(canvasRef: RefObject<HTMLCanvasElement | null>, ti
     const ctx = canvas.getContext('2d');
     if (!ctx) return;
 
-    renderMapAtTime(imageData, ctx, time);
+    renderMapAtTime({ imageData, ctx, time, alphaSize: 20 });
   }, [time, imageData, canvasRef]);
 }
