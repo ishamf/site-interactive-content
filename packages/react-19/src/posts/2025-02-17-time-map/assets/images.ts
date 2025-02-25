a80224e0fc2b5cd8efc3e5176e5d6070bb82d8d4
Isham Faizal <ishamf@users.noreply.github.com>
2025-02-25 19:56:31 +0700

    Remove unneeded ImageData

--- packages/react-19/src/posts/2025-02-17-time-map/assets/images.ts
+++ packages/react-19/src/posts/2025-02-17-time-map/assets/images.ts
@@ -1,6 +1,5 @@
 import dayImageSrc from './data/day.jpg?url';
 import nightImageSrc from './data/night.jpg?url';
-import { canvasWidth } from '../constants';
 
 async function loadImage(url: string) {
   const res = await fetch(url);
@@ -9,27 +8,11 @@ async function loadImage(url: string) {
 }
 
 export async function loadImageData() {
-  const [dayImage, nightImage] = await Promise.all([dayImageSrc, nightImageSrc].map(loadImage));
-
-  const dayImageCanvas = new OffscreenCanvas(canvasWidth, canvasWidth);
-  const nightImageCanvas = new OffscreenCanvas(canvasWidth, canvasWidth);
-
-  function createImageData(image: ImageBitmap, canvas: OffscreenCanvas) {
-    const ctx = canvas.getContext('2d');
-    if (!ctx) throw new Error('Could not get 2d context');
-    ctx.drawImage(image, 0, 0);
-
-    return ctx.getImageData(0, 0, canvas.width, canvas.height);
-  }
-
-  const dayImageData = createImageData(dayImage, dayImageCanvas);
-  const nightImageData = createImageData(nightImage, nightImageCanvas);
-
   const [dayImageBitmap, nightImageBitmap] = await Promise.all(
-    [dayImageData, nightImageData].map((data) => createImageBitmap(data))
+    [dayImageSrc, nightImageSrc].map(loadImage)
   );
 
-  return { dayImageData, nightImageData, dayImageBitmap, nightImageBitmap };
+  return { dayImageBitmap, nightImageBitmap };
 }
 
 export type MapImageData = Awaited<ReturnType<typeof loadImageData>>;
