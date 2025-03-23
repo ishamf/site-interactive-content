821424dc2adaf3bee18b81ca2ce255628a8c7345
Isham Faizal <ishamf@users.noreply.github.com>
2025-03-23 14:40:45 +0700

    Improve background

--- packages/react-19/src/posts/2025-02-17-time-map/TimeMap.tsx
+++ packages/react-19/src/posts/2025-02-17-time-map/TimeMap.tsx
@@ -23,8 +23,8 @@ export function TimeMap() {
   });
 
   return (
-    <div className="flex max-w-full flex-col p-4 gap-4 items-stretch justify-center md:flex-row md:items-start bg-neutral-50 dark:bg-neutral-950">
-      <div className="max-w-[170vh] flex-1 z-10 self-center md:self-start sticky top-0 py-4 flex items-stretch flex-col bg-neutral-50 dark:bg-neutral-950">
+    <div className="flex max-w-full flex-col p-4 gap-4 items-stretch justify-center md:flex-row md:items-start bg-neutral-50 dark:bg-neutral-900">
+      <div className="max-w-[170vh] flex-1 z-10 self-center md:self-start sticky top-0 py-4 flex items-stretch flex-col bg-neutral-50 dark:bg-neutral-900">
         <MapDisplay
           time={time.valueOf()}
           selectionDataById={selectionDataQuery.data?.selectionDataById ?? {}}
