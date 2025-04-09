360e8ea518596a227713bbe4c0a84888806a0a1f
Isham Faizal <ishamf@users.noreply.github.com>
2025-04-09 16:22:32 +0700

    Update tests

--- packages/react-19/tests/time-map.spec.ts
+++ packages/react-19/tests/time-map.spec.ts
@@ -7,23 +7,23 @@ test.use({
 
 test('local city is added', async ({ page }) => {
   await page.goto('http://localhost:5175/');
-  await expect(page.getByLabel('City').nth(0)).toHaveValue('Makassar');
+  await expect(page.getByLabel('City', { exact: true }).nth(0)).toHaveValue('Makassar');
 });
 
 test('time changes from local time', async ({ page }) => {
   await page.goto('http://localhost:5175/');
-  await expect(page.getByLabel('City').nth(0)).toHaveValue('Makassar');
+  await expect(page.getByLabel('City', { exact: true }).nth(0)).toHaveValue('Makassar');
 
-  await page.getByLabel('Time').nth(0).fill('04/09/2025 01:00 PM');
+  await page.getByLabel('Time', { exact: true }).nth(0).fill('04/09/2025 01:00 PM');
 
   await expect(page.locator('xif-time-map')).toContainText('9 April 2025 at 05:00 UTC');
 
-  await expect(page.locator('xif-time-map').first()).toHaveScreenshot();
+  await expect(page.locator('xif-time-map').first()).toHaveScreenshot({ maxDiffPixels: 10 });
 });
 
 test('time changes from specific city', async ({ page }) => {
   await page.goto('http://localhost:5175/');
-  await expect(page.getByLabel('City').nth(0)).toHaveValue('Makassar');
+  await expect(page.getByLabel('City', { exact: true }).nth(0)).toHaveValue('Makassar');
 
   const inputValues = await page
     .getByLabel('City')
@@ -34,14 +34,14 @@ test('time changes from specific city', async ({ page }) => {
 
   expect(londonIndex).toBeGreaterThan(-1);
 
-  await page.getByLabel('Time').nth(londonIndex).fill('04/09/2025 06:00 AM');
+  await page.getByLabel('Time', { exact: true }).nth(londonIndex).fill('04/09/2025 06:00 AM');
 
   await expect(page.locator('xif-time-map')).toContainText('9 April 2025 at 05:00 UTC');
 
   // During winter, London is UTC+0
-  await page.getByLabel('Time').nth(londonIndex).fill('01/09/2025 06:00 AM');
+  await page.getByLabel('Time', { exact: true }).nth(londonIndex).fill('01/09/2025 06:00 AM');
 
   await expect(page.locator('xif-time-map')).toContainText('9 January 2025 at 06:00 UTC');
 
-  await expect(page.locator('xif-time-map').first()).toHaveScreenshot();
+  await expect(page.locator('xif-time-map').first()).toHaveScreenshot({ maxDiffPixels: 10 });
 });
